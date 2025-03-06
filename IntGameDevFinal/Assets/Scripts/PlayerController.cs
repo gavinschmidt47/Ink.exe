c80a60f55322a9262ffbd8fc5cbc84c7d526bfaa
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-03-06 12:26:07 -0500

    PlayerController inputs

--- IntGameDevFinal/Assets/Scripts/PlayerController.cs
+++ IntGameDevFinal/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //GameObject
+
+    //UI
+
+    //Input
+    public InputActionAsset inputs;
+    private InputAction movement;
+    private Vector3 moveDir;
+
+    //Player Components
+    private Rigidbody rb;
+
+    void OnEnable()
+    {
+        movement = inputs.FindAction("Move");
+        movement.Enable();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        moveDir = movement.ReadValue<Vector2>();
+        rb.linearVelocity = new Vector3 (moveDir.x, 0, moveDir.y) * 100f * Time.deltaTime;
     }
 }
