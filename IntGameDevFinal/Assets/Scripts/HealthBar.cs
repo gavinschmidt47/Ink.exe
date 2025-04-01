8c9ac332a8400e12d677e7f8ea21ab67a00fb210
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-04-01 12:11:48 -0400

    Fixed Health bar

--- IntGameDevFinal/Assets/Scripts/HealthBar.cs
+++ IntGameDevFinal/Assets/Scripts/HealthBar.cs
@@ -5,13 +5,15 @@ public class HealthBar : MonoBehaviour
 {
     public Transform target;  // The target that the health bar will follow
     public Vector3 offset;    // Offset position from the target
-    public Image healthFill;  // The UI Image that represents the health fill
 
+
+    private Slider healthFill;  // The UI Image that represents the health fill
     private Camera mainCamera;
 
     void Start()
     {
         mainCamera = Camera.main;
+        healthFill = GetComponent<Slider>();
     }
 
     void Update()
@@ -21,12 +23,11 @@ public class HealthBar : MonoBehaviour
 
         // Make the health bar face the camera
         transform.LookAt(mainCamera.transform);
-        transform.Rotate(0, 180, 0);  // Rotate 180 degrees to face the camera correctly
     }
 
     // Function to update the health bar fill amount
     public void SetHealth(float currentHealth, float maxHealth)
     {
-        healthFill.fillAmount = currentHealth / maxHealth;
+        healthFill.value = currentHealth / maxHealth;
     }
 }
