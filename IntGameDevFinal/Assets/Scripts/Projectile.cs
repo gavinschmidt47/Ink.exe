f45cf38947a87aa2ea660a897d09b90b16e9dc52
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-04-03 12:18:41 -0400

    Projectile

---
+++ IntGameDevFinal/Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public EnemyController enemyController;
+    public Transform player;
+    public float speed = 10f;
+    public float life = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.LookAt(player.position);
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.linearVelocity = transform.forward * speed;
+
+        StartCoroutine(DestroyAfterTime(life));
+    }
+
+    IEnumerator DestroyAfterTime(float lifeTime)
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+    OnCollisionEnter
+}
