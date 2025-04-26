413e04a00ab2a8de4345bab3d29644a32b8780a8
Jelios04 <jadyn.englett@gmail.com>
2025-04-26 12:00:21 -0400

    Level Up Active
    Sets up level up screen to be shown.

--- IntGameDevFinal/Assets/LevelUp.cs
+++ IntGameDevFinal/Assets/LevelUp.cs
@@ -5,18 +5,24 @@ using UnityEngine.InputSystem;
 public class LevelUp : MonoBehaviour
 {
     public GameObject levelUp;
-    public GameObject pausePanel; 
+    public GameObject xpBar;
+    public GameObject levelPrompt;
+    public GameObject pausePanel;
     
+    public bool xpFull;
+
     private bool paused;
+    private bool level;
     private bool open = false;
     public static bool IsLevelOpen { get; private set; }
     public static bool IsPauseOpen { get; private set; }
+    public static int IsXPFull { get; private set; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         levelUp.SetActive(false); // Hide the panel at start
-        //pausemenu.SetActive(false); // Hide the panel at start
+        levelPrompt.SetActive(false); // Hide the panel at start
 
         //Set initial values
         Time.timeScale = 1;
@@ -24,11 +30,18 @@ public class LevelUp : MonoBehaviour
         pausePanel.SetActive(false);
     }
 
+    public void ActivateLevelPrompt()
+    {
+        level = true;
+        levelPrompt.SetActive(true);
+    }
+
     public void ToggleLevelUI()
     {
         open = !open;
         IsLevelOpen = open;
         levelUp.SetActive(open);
+        levelPrompt.SetActive(false);
     }
 
     public void CloseLevelUI()
@@ -68,12 +81,54 @@ public class LevelUp : MonoBehaviour
         }
     }
 
+    public void LevelOpen(InputAction.CallbackContext context)
+    {
+        //If correct state pressed
+        //if (!context.started) return;
+
+        //Freeze time scale, show UI, and set level state
+        if (level)
+        {
+            //Set unpaused values
+            Time.timeScale = 1;
+            levelUp.SetActive(false);
+            level = false;
+
+            //Disable cursor
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Debug.Log("Level Up Opened");
+            //Set level values
+            Time.timeScale = 0;
+            levelUp.SetActive(true);
+            levelPrompt.SetActive(false);
+            level = true;
+
+            //Enable cursor
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             Pause(new InputAction.CallbackContext()); // Call the Pause method with a new context
+        }
+
+        if(xpFull)
+        {
+            ActivateLevelPrompt();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && !level)
+        {
+            LevelOpen(new InputAction.CallbackContext()); // Call the LevelOpen method with a new context
         }
     }
 }
