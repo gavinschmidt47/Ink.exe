b2494024ab40f29ebd8f91e48b22344eda59b15a
SDrastal <sa122032@ucf.edu>
2025-04-29 01:26:39 -0400

    Virus fix, and now you can do build of it

--- IntGameDevFinal/Assets/Scripts/XMLGameSaveManager.cs
+++ IntGameDevFinal/Assets/Scripts/XMLGameSaveManager.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
-using UnityEditor.Overlays;
 using UnityEngine;
 
 public class XMLGameSaveManager : MonoBehaviour
